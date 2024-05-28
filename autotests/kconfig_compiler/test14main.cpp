97a79942f3f42d62bfb7361a17bb79ee09107d2b
Nicolas Fella <nicolas.fella@gmx.de>
2024-05-28 22:19:27 +0200

    kconfig_compiler: Allow generating QML type registration macros
    This adds a new option (QmlRegistration) that generates the needed code for declarative QML type registration
    
    When combined with the existing singleton option a QML singleton is registered

---
+++ autotests/kconfig_compiler/test14main.cpp
@@ -0,0 +1,12 @@
+/*
+    SPDX-FileCopyrightText: 2003 Cornelius Schumacher <schumacher@kde.org>
+
+    SPDX-License-Identifier: MIT
+*/
+#include "test14.h"
+
+int main(int, char **)
+{
+    Test14 t = Test14();
+    return 0;
+}
